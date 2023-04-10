ad19847842c416a56291b48b73c3cc132d7f7339
Dallin Hutchison <dallinhutchison@gmail.com>
2023-04-10 00:45:57 -0600

    made the website look a lot nicer by using tailwindcss

--- gpt_interface/app.py
+++ gpt_interface/app.py
@@ -4,9 +4,9 @@ from flask import request
 from inference import get_answer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+MODEL = None
+TOKENIZER = None
 app = Flask(__name__)
-model = AutoModelForCausalLM.from_pretrained("training/training-one")
-tokenizer = AutoTokenizer.from_pretrained("training/training-one")
 
 
 @app.route("/")
@@ -18,5 +18,15 @@ def hello():
 def request_answer():
     question = request.form["question"]
 
-    answer = get_answer(question, model, tokenizer)
+    if MODEL is None or TOKENIZER is None:
+        return "Model not loaded"
+
+    answer = get_answer(question, MODEL, TOKENIZER)
     return answer
+
+
+if __name__ == "__main__":
+    MODEL = AutoModelForCausalLM.from_pretrained("training/model")
+    TOKENIZER = AutoTokenizer.from_pretrained("training/model")
+
+    app.run(debug=False)
